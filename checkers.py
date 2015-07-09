a983d69865c50ff5f95a898383836d442c58876d
jeffthemaximum <frey.maxim@gmail.com>
2015-07-09 13:26:27 -0400

    added basic user prompt function

--- checkers.py
+++ checkers.py
@@ -6,7 +6,7 @@ def display(board):
 def initial_board():
     top = [['x' if col % 2 == row % 2 else ' '
             for col in range(8)]
-          for row in range(3)]
+           for row in range(3)]
 
     middle = [[' '] * 8 for x in range(2)]
 
@@ -20,6 +20,20 @@ def move(board, row, col, row2, col2):
     board[row2][col2] = board[row][col]
     board[row][col] = ' '
 
+def prompt_user():
+  print ("Which piece you want to move!?")
+  old_user_column = int(raw_input("Row: "))
+  old_user_row = int(raw_input("Column: "))
+
+  print("Where you wanna move it!?")
+  new_spot_row = int(raw_input("Row: "))
+  new_spot_col = int(raw_input("Column: "))
+  
+  return [old_user_row, old_user_column, new_spot_row, new_spot_col]
+  #some sort of checking in here?
+
+
 board = initial_board()
 
 display(board)
+print(prompt_user())
