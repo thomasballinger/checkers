214e929580f8cbd11e854c852ab58f023ec30044
Thomas Ballinger <thomasballinger@gmail.com>
2015-07-09 11:28:51 -0400

    Put initialization in function

--- checkers.py
+++ checkers.py
@@ -3,18 +3,19 @@ def display(board):
     for row in board:
         print row
 
-
-
-top = [['x' if col % 2 == row % 2 else ' '
-        for col in range(8)]
-      for row in range(3)]
-
-middle = [[' '] * 8 for x in range(2)]
-
-bottom = [['o' if col % 2 != row % 2 else ' '
-           for col in range(8)]
+def initial_board():
+    top = [['x' if col % 2 == row % 2 else ' '
+            for col in range(8)]
           for row in range(3)]
 
-board = top + middle + bottom
+    middle = [[' '] * 8 for x in range(2)]
+
+    bottom = [['o' if col % 2 != row % 2 else ' '
+               for col in range(8)]
+              for row in range(3)]
+    board = top + middle + bottom
+    return board
+
+board = initial_board()
 
 display(board)
